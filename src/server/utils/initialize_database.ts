670ab19006858ef5c52cd4cbc513c026676e8f50
Daaaai0809 <dai.tsuruga@80and.co>
2025-03-16 16:43:33 +0900

    feature_itemにidx

--- src/server/utils/initialize_database.ts
+++ src/server/utils/initialize_database.ts
@@ -1,7 +1,19 @@
 import fs from 'node:fs/promises';
+import { Database } from 'sqlite3';
 
 import { DATABASE_PATH, DATABASE_SEED_PATH } from './database_paths';
 
 export const initializeDatabase = async () => {
   await fs.copyFile(DATABASE_SEED_PATH, DATABASE_PATH);
+
+  // product.idにインデックスを追加
+  const db = new Database(DATABASE_PATH);
+  db.serialize(() => {
+    db.run('CREATE INDEX idx_feature_id ON feature_item (product_id)', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+  db.close();
 };
