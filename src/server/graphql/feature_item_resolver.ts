81be8b9b7d2078f095443a478e189a4d2738aba9
Daaaai0809 <dai.tsuruga@80and.co>
2025-03-16 16:12:36 +0900

    バックエンドの最適化

--- src/server/graphql/feature_item_resolver.ts
+++ src/server/graphql/feature_item_resolver.ts
@@ -1,9 +1,20 @@
-import type { FeatureItem } from '../../model/feature_item';
+import DataLoader from 'dataloader';
+import { dataSource } from '../data_source';
 
 import type { GraphQLModelResolver } from './model_resolver';
+import { Product } from '../../model/product';
+import { FeatureItem } from '../../model/feature_item';
 
 export const featureItemResolver: GraphQLModelResolver<FeatureItem> = {
-  product: (parent) => {
-    return parent.product;
-  },
+  product: async (parent) => await ProductLoader.load(parent.id),
 };
+
+const ProductLoader = new DataLoader(async (ids: readonly number[]) => {
+  const products = await dataSource
+    .createQueryBuilder(Product, 'product')
+    .whereInIds(ids)
+    .select(['product.id', 'product.name', 'product.price', 'product.description'])
+    .getMany();
+
+  return ids.map((id) => products.find((product) => product.id === id)) as Product[];
+});
